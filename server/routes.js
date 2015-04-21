cf6e503564c5881f856f4514770149a2a1f3ab0a
gaurav14 <gaurav@cronj.com>
2015-04-21 14:18:57 +0530

    some modification

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,16 @@
 // Load modules
 
 var User      = require('./controller/user'),
-    Static    = require('./static'),
-    scraper      = require('./controller/WebScraper/scraper.js');
+    Static    = require('./static');
 
 // API Server Endpoints
 exports.endpoints = [
 
     { method: 'GET',  path: '/{somethingss*}', config: Static.get },
     { method: 'POST', path: '/user', config: User.create},
+    { method: 'POST', path: '/login', config: User.login},
+    { method: 'POST', path: '/forgotPassword', config: User.forgotPassword},
     { method: 'POST', path: '/verifyEmail', config: User.verifyEmail},
-    { method: 'POST', path: '/GetData', config: scraper.getdata},
-   
+    { method: 'POST', path: '/resendVerificationEmail', config: User.resendVerificationEmail}
+  
 ];
